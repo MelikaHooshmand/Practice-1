5057809ff99189640f0285da67f61463e6ad8a2a
Melika <melikahooshmadd@gmail.com>
2021-08-25 04:25:15 -0700

    first commit

--- js/third.js
+++ js/third.js
@@ -4,12 +4,21 @@ const numberTwo = prompt('Please enter second number');
 
 const numberThree = prompt('Please enter third number');
 
-if(Number(numberOne > numberTwo)){
+
+if(Number(numberOne) > Number(numberTwo) && Number(numberOne) > Number(numberThree)){
+
     console.log('max =' +numberOne);
+
 }
-else if(Number(numberTwo > numberThree)){
+
+else if(Number(numberTwo) > numberThree){
+
     console.log('max =' +numberTwo);
+
 }
+
 else{
+
     console.log('max =' +numberThree);
+
 }
